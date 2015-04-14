9632e109eb79899b22d2a7c0c571d42fbfc7192f
Sharmeenjahan <jahans20@students.rowan.edu>
2015-04-14 13:02:54 -0400

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -2,6 +2,8 @@
 import java.sql.*;
 import java.util.Date;
 
+import org.bson.types.ObjectId;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
@@ -9,13 +11,11 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
-import org.bson.types.ObjectId;
-
 /**
 *
 *@author Drew Whittaker
-*@author John Cutsavage
-*@author Sharmeen Jahan
+* @author John Cutsavage
+* @author Sharmeen Jahan
 *
 * This class implements both MySQL and MongoDB APIs
 * in order to convert an example employee MySQL database
@@ -53,9 +53,7 @@ public class Converter {
 
 	MongoClient mongoClient;
 	DB db;
-	DBCollection eColl;	//collection variable to get the employees tables from database.
-	DBCollection tColl; //collection variable to get the titles tables from database.
-	DBCollection sColl; //collection variable to get the salaries tables from database.
+	DBCollection dColl, dmColl, eColl, tColl, sColl;	//collection variable to get the tables from database.
 	
 	//Initialize the connection and get the database from mongodb instance.
    public void initMongoConnection(){
@@ -76,140 +74,205 @@ public class Converter {
     	  System.out.println(e.getMessage());
       }
    }
-  
-	
+  	
+	public void copyDepartments(){
+		try {
+		// Run a select query of all entries in departments table from MySQL
+		Statement statement = con.createStatement();
+		ResultSet res = statement.executeQuery("SELECT * FROM departments");
+		
+		// Create new table/collection of departments
+		dColl= db.getCollection("departments");
+		dColl.remove(new BasicDBObject());
+		BasicDBObject copyDocument = new BasicDBObject();
+		// Iterate through the departments, capture the information, and copy it to MongoDB
+		while(res.next()) {
+		String dept_no = res.getString("dept_no");
+		String dept_name = res.getString("dept_name");
+		
+
+		copyDocument.put("dept_no", dept_no);
+		copyDocument.put("dept_name", dept_name);
+		dColl.insert(copyDocument);
+		
+		dept_name= "";
+		copyDocument.clear();
+		
+		}
+		
+		res.close();
+		res = null;
+		copyDocument = null;
+		}
+		catch (Exception e) {
+		e.printStackTrace();
+		}
+		}
 	
 	public void copyEmployees(){
 		try {
-			// Run a select query of all entries in employees table from MySQL
-			Statement statement = con.createStatement();
-			ResultSet res = statement.executeQuery("SELECT * FROM employees");
+		// Run a select query of all entries in departments table from MySQL
+		Statement statement = con.createStatement();
+		ResultSet res = statement.executeQuery("SELECT * FROM employees");
 		
-			// Create new table/collection of employees
-			eColl= db.getCollection("employees");
+		// Create new table/collection of departments
+		eColl= db.getCollection("employees");
+		eColl.remove(new BasicDBObject());
+		//sColl.remove(new BasicDBObject());
+		//tColl.remove(new BasicDBObject());
+		sColl= db.getCollection("salaries");
+		tColl=db.getCollection("titles");
 		
-			// Iterate through the employees, capture their information, and copy it to MongoDB
-			while(res.next()) {
-				int empNum = res.getInt("emp_no");
-				Date birthDate = res.getDate("birth_date");
-				String firstName = res.getString("first_name");
-				String lastName = res.getString("last_name");
-				Object gender = res.getObject("gender");
-				Date hireDate = res.getDate("hire_date");
-
-				BasicDBObject copyDocument = new BasicDBObject();
-
-				copyDocument.put("emp_no", empNum);
-				copyDocument.put("birth_date", birthDate);
-				copyDocument.put("first_name", firstName);
-				copyDocument.put("last_name", lastName);
-				copyDocument.put("gender", gender);
-				copyDocument.put("hire_date", hireDate);
-				
-				eColl.insert(copyDocument);
-				
-				// Next, convert the current employee's job title and salary
-				ObjectId id = (ObjectId) copyDocument.get("_id"); //Use this ID for reference in the next two methods
-				copyTitle(empNum, id);
-				copySalary(empNum, id);
-			}
+		int emp_no;
+		Date birthDate, hireDate;
+		String firstName, lastName;
+		Object gender = null;
+		BasicDBObject copyDocument = new BasicDBObject();
+		
+		// Iterate through the departments, capture the information, and copy it to MongoDB
+		while(res.next()) {
+			emp_no = res.getInt("emp_no");
+			birthDate = res.getDate("birth_date");
+			firstName = res.getString("first_name");
+			lastName = res.getString("last_name");
+			gender = res.getObject("gender");
+			hireDate = res.getDate("hire_date");
+			
+			copyDocument.put("emp_no", emp_no);
+			copyDocument.put("birth_date", birthDate);
+			copyDocument.put("first_name", firstName);
+			copyDocument.put("last_name", lastName);
+			copyDocument.put("gender", gender);
+			copyDocument.put("hire_date", hireDate);
+			eColl.insert(copyDocument);	
+			
+			// Next, convert the current employee's job title and salary
+			ObjectId id = (ObjectId) copyDocument.get("_id"); //Use this ID for reference in the next two methods
+			copyTitle(emp_no, id);
+			copySalary(emp_no, id);
+			
+			firstName = "";
+			lastName = "";
+			copyDocument.clear();
+			
+		}
+		copyDocument = null;
+		res.close();
+		res= null;
 		}
 		catch (Exception e) {
-			e.printStackTrace();
+		e.printStackTrace();
 		}
-	}
-	
+		}
+
 	/**
-	 * Copy into the titles collection based on
-	 * an employee's number and the Object ID to reference back to
-	 * the employees collection. Similar to copyEmployees().
-	 * @param empNum: The value of the employee's emp_no
-	 * @param id: The ObjectId used by the employee's document
-	 */
+	* Copy into the titles collection based on
+	* an employee's number and the Object ID to reference back to
+	* the employees collection. Similar to copyEmployees().
+	* @param empNum: The value of the employee's emp_no
+	* @param id: The ObjectId used by the employee's document
+	*/
 	public void copyTitle(int empNum, ObjectId id){
 		try {
-			//Search for the salary of an employee by their emp_no
-			Statement statement = sqlConn.createStatement();
-			ResultSet res = statement.executeQuery("SELECT * FROM titles WHERE emp_no LIKE '" + empNum + "'");
-			
-			String title = res.getString("title");
-			Date from = res.getDate("from_date");
-			Date to = res.getDate("to_date");
-			
-			tColl= db.getCollection("titles");
-			
+			String title;
 			BasicDBObject copyDocument = new BasicDBObject();
 			
-			copyDocument.put("emp_no", id);
-			copyDocument.put("title", title);
-			copyDocument.put("from_date", from);
-			copyDocument.put("to_date", to);
 			
-			tColl.insert(copyDocument);
+			//Search for the salary of an employee by their emp_no
+			Statement statement = con.createStatement();
+			ResultSet res = statement.executeQuery("SELECT * FROM titles WHERE emp_no = " + Integer.toString(empNum));
+			while(res.next()) {
+				title = res.getString("title");
+				Date from = res.getDate("from_date");
+				Date to = res.getDate("to_date");
+				
+				
+				
+				
+				copyDocument.put("emp_no", id);
+				copyDocument.put("title", title);
+				copyDocument.put("from_date", from);
+				copyDocument.put("to_date", to);
+				
+				tColl.insert(copyDocument);
+				
+				copyDocument.clear();
+				title = "";
+			}
+			copyDocument=null;
+			res.close();
+			res= null;
 		}
 		catch (Exception e){
 			e.printStackTrace();
 		}
 	}
-	
 	/**
-	 * Copy into the salaries collection based on
-	 * an employee's number and the Object ID to reference back to
-	 * the employees collection. Similar to copyEmployees().
-	 * @param empNum: The value of the employee's emp_no
-	 * @param id: The ObjectId used by the employee's document
-	 */
+	* Copy into the salaries collection based on
+	* an employee's number and the Object ID to reference back to
+	* the employees collection. Similar to copyEmployees().
+	* @param empNum: The value of the employee's emp_no
+	* @param id: The ObjectId used by the employee's document
+	*/
 	public void copySalary(int empNum, ObjectId id){
 		try {
-			//Search for the salary of an employee by their emp_no
-			Statement statement = sqlConn.createStatement();
-			ResultSet res = statement.executeQuery("SELECT * FROM salaries WHERE emp_no LIKE '" + empNum + "'");
-			
-			String salary = res.getString("salary");
-			Date from = res.getDate("from_date");
-			Date to = res.getDate("to_date");
-			
-			sColl= db.getCollection("salaries");
-			
+			String salary;	
 			BasicDBObject copyDocument = new BasicDBObject();
 			
-			copyDocument.put("emp_no", id);
-			copyDocument.put("salary", salary);
-			copyDocument.put("from_date", from);
-			copyDocument.put("to_date", to);
 			
-			sColl.insert(copyDocument);
+			//Search for the salary of an employee by their emp_no
+			Statement statement = con.createStatement();
+			ResultSet res = statement.executeQuery("SELECT * FROM salaries WHERE emp_no = " + Integer.toString(empNum) );
+			
+			while(res.next()){
+				salary = res.getString("salary");
+				Date from = res.getDate("from_date");
+				Date to = res.getDate("to_date");
+				
+				
+				
+				
+				copyDocument.put("emp_no", id);
+				copyDocument.put("salary", salary);
+				copyDocument.put("from_date", from);
+				copyDocument.put("to_date", to);
+				sColl.insert(copyDocument);
+				
+				copyDocument.clear();
+				salary = "";
+				
+			}
+			copyDocument=null;
+			res.close();
+			res=null;
 		}
 		catch (Exception e){
-			e.printStackTrace();
+		e.printStackTrace();
 		}
 	}
-
-	
 	/**
 	* Closes the MySQL connection.
 	*/
 	public void closeSQLConnection(){
-		try{
-		con.close();
-		}
-		catch(SQLException e) {
-		e.printStackTrace();
-		}
+	try{
+	con.close();
 	}
-	
+	catch(SQLException e) {
+	e.printStackTrace();
+	}
+	}
 	/**
-	 * Removes employee database from Mongo
-	 */
+	* Removes employee database from Mongo
+	*/
 	public void removeDB(){
-		try {
-			db.drop();
-		}
-		catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+	try {
+	db.dropDatabase();
 	}
-	
+	catch (Exception e) {
+	System.out.println(e.getMessage());
+	}
+	}
+
 }
 
    

89a094ab3f1275c42da72fbcddc24a429dfc19f2
John Cutsavage <anikansky3@gmail.com>
2015-04-15 15:22:22 -0400

    Added STATUS_SUMMARY file (not completed). Added documentation to MysqlJacaAPI.java.

--- src/MysqlJavaAPI.java
+++ src/MysqlJavaAPI.java
@@ -4,7 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+/**
+ * @author John Cutsavage
+ * @author Sharmeen Jahan
+ * @author Drew Whittaker
+ * 
+ * This API is used to test the different performance
+ * aspects of MySQL, as well as the conversion from a MySQL
+ * database into a Mongo database.
+ */
 public class MysqlJavaAPI {
 
 	public static void main(String[] args) throws SQLException {

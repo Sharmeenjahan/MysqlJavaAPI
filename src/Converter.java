01c3861dc0a9baea0e0747009a6535fca85f0b7d
Sharmeenjahan <jahans20@students.rowan.edu>
2015-04-14 20:07:32 -0400

    Update Converter.java

--- src/Converter.java
+++ src/Converter.java
@@ -53,7 +53,7 @@ public class Converter {
 
 	MongoClient mongoClient;
 	DB db;
-	DBCollection dColl, dmColl, eColl, tColl, sColl;	//collection variable to get the tables from database.
+	DBCollection dColl, dmColl, deColl, eColl, tColl, sColl;	//collection variable to get the tables from database.
 	
 	//Initialize the connection and get the database from mongodb instance.
    public void initMongoConnection(){
@@ -74,7 +74,9 @@ public class Converter {
     	  System.out.println(e.getMessage());
       }
    }
-  	
+   /**
+	* Copy into the departments collection.
+	*/
 	public void copyDepartments(){
 		try {
 		// Run a select query of all entries in departments table from MySQL
@@ -109,6 +111,9 @@ public class Converter {
 		}
 		}
 	
+	/**
+	* Copy into the employees collection.
+	*/
 	public void copyEmployees(){
 		try {
 		// Run a select query of all entries in departments table from MySQL
@@ -122,6 +127,8 @@ public class Converter {
 		//tColl.remove(new BasicDBObject());
 		sColl= db.getCollection("salaries");
 		tColl=db.getCollection("titles");
+		dmColl=db.getCollection("dept_manager");
+		deColl= db.getCollection("dept_emp");
 		
 		int emp_no;
 		Date birthDate, hireDate;
@@ -150,7 +157,7 @@ public class Converter {
 			ObjectId id = (ObjectId) copyDocument.get("_id"); //Use this ID for reference in the next two methods
 			copyTitle(emp_no, id);
 			copySalary(emp_no, id);
-			
+			copyDeptEmp(emp_no, id);
 			firstName = "";
 			lastName = "";
 			copyDocument.clear();
@@ -164,6 +171,104 @@ public class Converter {
 		e.printStackTrace();
 		}
 		}
+	
+	/**
+	* Copy into the dept_emp collection based on
+	* an employee's number, and the Object ID to reference back to
+	* the employees collection.
+	* @param empNum: The value of the employee's emp_no
+	* @param eid: The ObjectId used by the employee's document
+	*/
+	public void copyDeptEmp(int emp_no,Object empId){
+		try {
+			
+			BasicDBObject copyDocument = new BasicDBObject();
+			String deptNo;
+			Date from, to;
+			DBObject findDept = new BasicDBObject();
+			Object deptId = null;
+			DBObject deptRec = new BasicDBObject();
+			
+			//Search for the salary of an employee by their emp_no
+			Statement statement = con.createStatement();
+			ResultSet res = statement.executeQuery("SELECT * FROM dept_emp where emp_no=" + Integer.toString(emp_no));
+			while(res.next()) {
+				//String empNo=res.getString("emp_no");
+				deptNo= res.getString("dept_no");
+				from = res.getDate("from_date");
+				to = res.getDate("to_date");
+				
+				
+				findDept.put("dept_no", deptNo);
+				deptRec=dColl.findOne(findDept);
+				deptId=deptRec.get("_id");
+				
+				
+				//Insert dept_emp collection
+				copyDocument.put("emp_no", empId);
+				copyDocument.put("dept_no" ,deptId);
+				copyDocument.put("from_date", from);
+				copyDocument.put("to_date", to);
+				
+				deColl.insert(copyDocument);
+				
+				copyDeptManager(deptNo, emp_no, deptId, empId);
+				copyDocument.clear();
+				
+			}
+			copyDocument=null;
+			res.close();
+			res= null;
+			deptNo= "";
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	* Copy into the dept_manager collection based on
+	* an employee's number, department's no and the Object IDs to reference back to
+	* the departments and employees collection.
+	* @param deptNum: The value of the employee's dept_no
+	* @param did: The ObjectId used by the department's document
+	* @param empNum: The value of the employee's emp_no
+	* @param eid: The ObjectId used by the employee's document
+	*/
+	
+	public void copyDeptManager( String deptNum, int empNum, Object dId, Object eId){
+		try {
+			
+			BasicDBObject copyDocument = new BasicDBObject();
+			Date from, to;
+			
+			//Search for the salary of an employee by their emp_no
+			Statement statement = con.createStatement();
+			ResultSet res = statement.executeQuery("SELECT * FROM dept_manager WHERE dept_no = '" + deptNum +"' && emp_no = " + Integer.toString(empNum));
+			while(res.next()) {
+				from = res.getDate("from_date");
+				to = res.getDate("to_date");
+				
+				copyDocument.put("dept_no", dId);
+				copyDocument.put("emp_no", eId);
+				copyDocument.put("from_date", from);
+				copyDocument.put("to_date", to);
+				
+				dmColl.insert(copyDocument);
+				
+				copyDocument.clear();
+				
+			}
+			copyDocument=null;
+			res.close();
+			res= null;
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	
 
 	/**
 	* Copy into the titles collection based on
@@ -176,19 +281,16 @@ public class Converter {
 		try {
 			String title;
 			BasicDBObject copyDocument = new BasicDBObject();
-			
+			Date from, to;
 			
 			//Search for the salary of an employee by their emp_no
 			Statement statement = con.createStatement();
 			ResultSet res = statement.executeQuery("SELECT * FROM titles WHERE emp_no = " + Integer.toString(empNum));
 			while(res.next()) {
 				title = res.getString("title");
-				Date from = res.getDate("from_date");
-				Date to = res.getDate("to_date");
-				
-				
-				
-				
+				from = res.getDate("from_date");
+				to = res.getDate("to_date");
+							
 				copyDocument.put("emp_no", id);
 				copyDocument.put("title", title);
 				copyDocument.put("from_date", from);
@@ -228,8 +330,6 @@ public class Converter {
 				salary = res.getString("salary");
 				Date from = res.getDate("from_date");
 				Date to = res.getDate("to_date");
-				
-				
 				
 				
 				copyDocument.put("emp_no", id);
